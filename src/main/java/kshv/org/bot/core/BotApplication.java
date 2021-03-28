e6220d701a8adaf713dcc269d103ef2ddbe778e1
codevarius <koshaevk@gmail.com>
2021-03-28 19:25:42 +0300

    added dev profile added access.token property

---
+++ src/main/java/kshv/org/bot/core/BotApplication.java
@@ -0,0 +1,15 @@
+package kshv.org.bot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class BotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+
+}
